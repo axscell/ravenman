36c2806348ad4735e7462a35a5d0c8919692683f
0xshervin <58803767+0xShervin@users.noreply.github.com>
2024-03-15 20:03:52 +0330

    removed and renamed some files and added not completed files

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,12 +1,8 @@
-mod game;
-mod spinning_cube;
-
-use gdnative::prelude::{godot_init, InitHandle};
+use gdnative::prelude::*;
 
 // Function that registers all exposed classes to Godot
 fn init(handle: InitHandle) {
-    handle.add_class::<game::Game>();
-    handle.add_class::<spinning_cube::SpinningCube>();
+    
 }
 
 // macros that create the entry-points of the dynamic library.
